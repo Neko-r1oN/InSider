da205325d52ce00ebf8a18007115ca8c9a92b8e4
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-02-26 14:13:03 +0900

    川口 : 待機シーン完成

--- Assets/USERS/Kawaguti/02_Scripts/Standby/StandBy.cs
+++ Assets/USERS/Kawaguti/02_Scripts/Standby/StandBy.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StandBy : MonoBehaviour
 {
+    private float _repeatSpan;    //ŒJ‚è•Ô‚·ŠÔŠu
+    private float _timeElapsed;   //Œo‰ßŽžŠÔ
+
+    [SerializeField] Text StandText;
     [SerializeField] GameObject OKbutton;
     [SerializeField] GameObject Cancelbutton;
     [SerializeField] GameObject OKIcon;
+    AudioSource audio;
+    [SerializeField] AudioClip ClickSound;
 
     private void Start()
     {
+      
+        audio = GetComponent<AudioSource>();
         OKIcon.SetActive(false);
         OKbutton.SetActive(true);
         Cancelbutton.SetActive(false);
 
+    }
+    private void Update()
+    {
+       
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return))
+        {
+
+            audio.PlayOneShot(ClickSound);
+
+        }
+
+       
     }
     public void OKButton()
     {
