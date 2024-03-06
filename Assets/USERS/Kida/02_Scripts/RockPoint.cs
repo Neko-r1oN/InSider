b1b5b460011c65dbbee8cd7c72ec6fe4bcbc08ce
S-JOBI23-060\student <231122@st.yoshida-g.ac.jp>
2024-03-06 16:29:51 +0900

    落石の生成

--- Assets/USERS/Kida/02_Scripts/RockPoint.cs
+++ Assets/USERS/Kida/02_Scripts/RockPoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RockPoint : MonoBehaviour
 {
+    [SerializeField] GameObject fallingLock;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class RockPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject fallLock = Instantiate(fallingLock);
+
+        }
     }
 }
